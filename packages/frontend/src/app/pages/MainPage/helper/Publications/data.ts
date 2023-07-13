2fff894e68a7f5e5b033ef20829b3d9a131bacfe
orangeBisquit <62883132+orangeBisquit@users.noreply.github.com>
2023-07-13 20:43:45 +0600

    main-page achievements and publications section added and adapted. box-shadowed component added

---
+++ packages/frontend/src/app/pages/MainPage/helper/Publications/data.ts
@@ -0,0 +1,30 @@
+import CoinTelegraphLogo from '../../../../../assets/img/MainPage/publications/cointelegraph-logo.svg'
+import FileCoinLogo from '../../../../../assets/img/MainPage/publications/filecoin-logo.svg'
+import { SlideProps } from '../../Blocks/Publications/Publications'
+
+export const PublicationsData: SlideProps[] = [
+  {
+    titleLogo: FileCoinLogo,
+    tag: 'FileCoin',
+    text: 'Filecoin Virtual Machine (FVM) Builder Cohort Launches to Mainnet',
+    href: 'https://filecoin.io/blog/posts/filecoin-virtual-machine-fvm-builder-cohort-launches-to-mainnet/',
+  },
+  {
+    titleLogo: FileCoinLogo,
+    tag: 'FileCoin',
+    text: '100 Days of FVM',
+    href: 'https://filecoin.io/blog/posts/100-days-of-fvm/',
+  },
+  {
+    titleLogo: CoinTelegraphLogo,
+    tag: 'FileCoin',
+    text: 'This platform’s EFT standard makes trading downloadable encrypted content accessible',
+    href: 'https://cointelegraph.com/news/this-platforms-eft-standard-makes-trading-downloadable-encrypted-content-accessible',
+  },
+  {
+    titleLogo: CoinTelegraphLogo,
+    tag: 'FileCoin',
+    text: 'Revolutionizing digital privacy: Meet the FileBunnies — The first NFTs with Encrypted FileToken',
+    href: 'https://cointelegraph.com/press-releases/revolutionizing-digital-privacy-meet-the-filebunnies-the-first-nfts-with-encrypted-filetoken',
+  },
+]
