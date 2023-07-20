3889937f82f3b77108750862bedf59c3d23309f4
LewinUp <66261148+Aleksey-Levin@users.noreply.github.com>
2023-07-20 14:15:23 +0300

    Update NFTDealActions.tsx

--- packages/frontend/src/app/components/NFT/NFTDeal/NFTDealActions/NFTDealActions.tsx
+++ packages/frontend/src/app/components/NFT/NFTDeal/NFTDealActions/NFTDealActions.tsx
@@ -2,7 +2,7 @@ import { Tooltip } from '@nextui-org/react'
 import { utils } from 'ethers'
 import { observer } from 'mobx-react-lite'
 import React, { FC, useMemo } from 'react'
-
+import { transferPermissions } from '../../../../utils/transfer/status'
 import { styled } from '../../../../../styles'
 import { Order, Transfer } from '../../../../../swagger/Api'
 import { mark3dConfig } from '../../../../config/mark3d'
@@ -51,13 +51,13 @@ export const NFTDealActions: FC<NFTDealActionsProps> = observer(({
   const collectionAddressNormalized = tokenFullId?.collectionAddress && utils.getAddress(tokenFullId?.collectionAddress)
   const fileBunniesAddressNormalized = utils.getAddress(mark3dConfig.fileBunniesCollectionToken.address)
   const isFileBunnies = collectionAddressNormalized === fileBunniesAddressNormalized
-
+const permission = status.buyer
   const fileBunniesText = useMemo(() => {
-    return isFileBunnies && !transfer ? (+tokenFullId.tokenId >= 7000 ? 'The secondary market will open on August 28th' : 'Unlocked 23.12.2023') : ''
+    return isFileBunnies && (!transfer || permissions.canFinalize(transfer)) ? (+tokenFullId.tokenId >= 7000 ? 'The secondary market will open on August 28th' : 'Unlocked 23.12.2023') : ''
   }, [isFileBunnies, transfer, tokenFullId])
 
   const isDisabledFileBunnies = useMemo(() => {
-    return isFileBunnies && !transfer
+    return isFileBunnies && (!transfer || permissions.canFinalize(transfer))
   }, [isFileBunnies, transfer])
 
   return (
