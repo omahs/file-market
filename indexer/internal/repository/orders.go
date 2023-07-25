23e692171d17a5ddce7958f2cc259a52be3b3e83
w8noot <ynifor63@gmail.com>
2023-07-25 15:36:49 +0500

    sales volume query fix

--- indexer/internal/repository/orders.go
+++ indexer/internal/repository/orders.go
@@ -448,7 +448,7 @@ func (p *postgres) GetActiveOrder(ctx context.Context, tx pgx.Tx, contractAddres
 func (p *postgres) GetSalesVolumeByCollection(ctx context.Context, tx pgx.Tx, address common.Address) (*big.Int, error) {
 	// language=PostgreSQL
 	query := `
-		SELECT SUM(o.price::BIGINT)::VARCHAR(255)
+		SELECT SUM(o.price::NUMERIC)::VARCHAR(255)
 		FROM orders o
 		JOIN transfers t on 
 		    o.transfer_id = t.id and 
