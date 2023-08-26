a33029a563d980b67b1d1f7d87458d63cf5e68fa
w8noot <ynifor63@gmail.com>
2023-08-27 02:16:19 +0500

    fix

--- authserver/migrations/20230827015228_profile_discord.sql
+++ authserver/migrations/20230827015228_profile_discord.sql
@@ -2,12 +2,12 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 -- +goose StatementEnd
-ALTER TABLE auth_server.public.user_profiles
+ALTER TABLE public.user_profiles
     ADD COLUMN discord TEXT UNIQUE;
 
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
 -- +goose StatementEnd
-ALTER TABLE auth_server.public.user_profiles
+ALTER TABLE public.user_profiles
     DROP COLUMN discord;
